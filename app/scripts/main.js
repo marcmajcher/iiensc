86c4316d405cac9f38b2cf4c821c4369805fb92c
Marc Majcher <majcher@gmail.com>
2015-11-03 18:09:19 -0600

    data parsing and institution selection

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,30 @@
 (function() {
 	var datapath = '/data/data.csv';
+	var data = {};
+
+	function populate(event) {
+		var key = event.target.value;
+		if (key in data) {
+			console.log(event.target.value);
+			console.log(data[event.target.value]);
+		}
+	}
+
 	Papa.parse(datapath, {
 		download: true,
 		complete: function(results) {
-			console.log("CVS parsing complete");
-			console.log(results);
+			results.data.splice(0, 2);  // ditch the first two header rows
+			results.data.splice(-1, 1); // trim the last footer row
+
+			var schools = results.data.map(function(arr) { return arr[0]; });
+			results.data.forEach(function(arr) { data[arr[0]] = arr.slice(1); });
+
+			$('#institution-name').autocomplete({
+				source: schools,
+				change: populate,
+				select: populate
+			});
+
 		}
 	});
 })();
